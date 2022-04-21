cb10653aaa1740abfa377a7edcadf3a12d57093c
michael <mcbrunette33@gmail.com>
2022-04-21 13:57:37 -0700

    added movies

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/weather', async (req, res) => {
     let weatherApiUrl = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&land=en&lat=${lat}&lon=${lon}&days=5`;
     let weatherData = await axios.get(weatherApiUrl);
     console.log(weatherData.data.data);
-    let weatherdata2 = weatherData.data.data.map(p => new Forecast (p));
+    let weatherdata2 = weatherData.data.data.map(p => new Forecast(p));
     res.status(200).send(weatherdata2);
     // let locationData = weatherData.find(data => data.city_name.toLowerCase() === locationInput.toLowerCase());
     // console.log(locationData);
@@ -34,6 +34,18 @@ app.get('/weather', async (req, res) => {
     console.log(error);
   }
 });
+
+app.get('/movie', async (req, res) => {
+  try {
+    let locationCity = req.query.locationCity;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${locationCity}&page=1&include_adult=false`;
+    let movieLog = await axios.get(movieUrl);
+    let movieArr = movieLog.data.results.map(object => new Movies(object));
+    res.status(200).send(movieArr);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 app.get('*', (req, res) => {
   throw new Error('Not working!');
@@ -52,4 +64,13 @@ class Forecast {
     this.description = data.weather.description
   }
 }
+
+class Movies {
+  constructor(dataset){
+    this.title = dataset.title;
+    this.popularity = dataset.popularity;
+    this.overview = dataset.overview;
+  }
+}
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
